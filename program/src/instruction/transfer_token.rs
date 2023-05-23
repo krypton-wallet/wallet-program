a62cca40468ecb80dab4529b8ffee55204d6197c
nil <nilz3ro@protonmail.com>
2023-05-23 13:55:10 -0500

    fix transfer_token

--- program/src/instruction/transfer_token.rs
+++ program/src/instruction/transfer_token.rs
@@ -1,62 +1,83 @@
-use solana_program::pubkey::Pubkey;
+use crate::{
+    error::KryptonError,
+    state::{get_profile_pda, PDA_SEED},
+};
+use solana_program::{
+    account_info::{next_account_info, AccountInfo},
+    entrypoint::ProgramResult,
+    msg,
+    program::invoke_signed,
+    program_error::ProgramError,
+    program_pack::Pack,
+    pubkey::Pubkey,
+};
+use spl_token::{self, state::Account as TokenAccount};
 
-pub fn process_transfer_token(program_id: &Pubkey, accounts: &[AccountInfo], args: TransferTokenArgs) -> ProgramResult {
-                msg!("Instruction: TransferToken");
+use super::TransferTokenArgs;
 
-                let profile_info = next_account_info(account_info_iter)?;
-                let authority_info = next_account_info(account_info_iter)?;
-                let token_account_info = next_account_info(account_info_iter)?;
-                let dest_token_account_info = next_account_info(account_info_iter)?;
-                let token_program = next_account_info(account_info_iter)?;
+pub fn process_transfer_token(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    args: TransferTokenArgs,
+) -> ProgramResult {
+    let mut account_info_iter = accounts.into_iter();
+    msg!("Instruction: TransferToken");
 
-                // ensure authority_info is signer
-                if !authority_info.is_signer {
-                    return Err(KryptonError::NotSigner.into());
-                }
+    let profile_info = next_account_info(&mut account_info_iter)?;
+    let authority_info = next_account_info(&mut account_info_iter)?;
+    let token_account_info = next_account_info(&mut account_info_iter)?;
+    let dest_token_account_info = next_account_info(&mut account_info_iter)?;
+    let token_program = next_account_info(&mut account_info_iter)?;
 
-                // ensure token_account_info and dest_token_account_info are writable
-                if !token_account_info.is_writable || !dest_token_account_info.is_writable {
-                    return Err(KryptonError::NotWriteable.into());
-                }
+    // ensure authority_info is signer
+    if !authority_info.is_signer {
+        return Err(KryptonError::NotSigner.into());
+    }
 
-                // ensure profile_info PDA corresponds to authority_info
-                let (profile_pda, bump_seed) = get_profile_pda(authority_info.key, program_id);
-                if profile_pda != *profile_info.key {
-                    return Err(ProgramError::InvalidSeeds);
-                }
+    // ensure token_account_info and dest_token_account_info are writable
+    if !token_account_info.is_writable || !dest_token_account_info.is_writable {
+        return Err(KryptonError::NotWriteable.into());
+    }
 
-                msg!("account checks complete");
+    // ensure profile_info PDA corresponds to authority_info
+    let (profile_pda, bump_seed) = get_profile_pda(authority_info.key, program_id);
+    if profile_pda != *profile_info.key {
+        return Err(ProgramError::InvalidSeeds);
+    }
 
-                // unpack token account to get amount
-                let token_account = TokenAccount::unpack(&token_account_info.try_borrow_data()?)?;
-                msg!("amount: {}, total: {}", args.amount, token_account.amount);
+    msg!("account checks complete");
 
-                // ensure ATA has enough tokens to transfer
-                if token_account.amount < args.amount {
-                    return Err(ProgramError::InsufficientFunds.into());
-                }
+    // unpack token account to get amount
+    let token_account = TokenAccount::unpack(&token_account_info.try_borrow_data()?)?;
+    msg!("amount: {}, total: {}", args.amount, token_account.amount);
 
-                msg!("transfering mint...");
-                let transfer_ix = transfer(
-                    token_program.key,
-                    token_account_info.key,
-                    dest_token_account_info.key,
-                    &profile_pda,
-                    &[&profile_pda],
-                    args.amount,
-                )?;
-                invoke_signed(
-                    &transfer_ix,
-                    &[
-                        token_program.clone(),
-                        token_account_info.clone(),
-                        dest_token_account_info.clone(),
-                        authority_info.clone(),
-                        profile_info.clone(),
-                    ],
-                    &[&[PDA_SEED, authority_info.key.as_ref(), &[bump_seed]]],
-                )?;
-                msg!("finished transfer of mint");
+    // ensure ATA has enough tokens to transfer
+    if token_account.amount < args.amount {
+        return Err(ProgramError::InsufficientFunds.into());
+    }
 
-                Ok(())
+    msg!("transfering mint...");
+    let transfer_ix = spl_token::instruction::transfer(
+        token_program.key,
+        token_account_info.key,
+        dest_token_account_info.key,
+        &profile_pda,
+        &[&profile_pda],
+        args.amount,
+    )?;
+
+    invoke_signed(
+        &transfer_ix,
+        &[
+            token_program.clone(),
+            token_account_info.clone(),
+            dest_token_account_info.clone(),
+            authority_info.clone(),
+            profile_info.clone(),
+        ],
+        &[&[PDA_SEED, authority_info.key.as_ref(), &[bump_seed]]],
+    )?;
+    msg!("finished transfer of mint");
+
+    Ok(())
 }
--- program/src/processor.rs
+++ program/src/processor.rs
@@ -8,7 +8,7 @@ use solana_program::{
     program_error::ProgramError,
     program_pack::Pack,
     pubkey::Pubkey,
-    system_instruction::{assign},
+    system_instruction::assign,
     system_program::check_id,
 };
 use spl_token::{
@@ -18,12 +18,12 @@ use spl_token::{
 
 use crate::{
     error::KryptonError,
+    instruction::transfer_token,
     state::{get_profile_pda, verify_recovery_state, ProfileHeader},
 };
 use crate::{
     instruction::{initialize_wallet, KryptonInstruction},
     state::{Guardian, DATA_LEN, MAX_GUARDIANS, PDA_SEED},
-
 };
 
 pub struct Processor {}
@@ -44,64 +44,7 @@ impl Processor {
                 initialize_wallet::process_initialize_wallet(program_id, accounts, args)
             }
             KryptonInstruction::TransferToken(args) => {
-                msg!("Instruction: TransferToken");
-
-                let profile_info = next_account_info(account_info_iter)?;
-                let authority_info = next_account_info(account_info_iter)?;
-                let token_account_info = next_account_info(account_info_iter)?;
-                let dest_token_account_info = next_account_info(account_info_iter)?;
-                let token_program = next_account_info(account_info_iter)?;
-
-                // ensure authority_info is signer
-                if !authority_info.is_signer {
-                    return Err(KryptonError::NotSigner.into());
-                }
-
-                // ensure token_account_info and dest_token_account_info are writable
-                if !token_account_info.is_writable || !dest_token_account_info.is_writable {
-                    return Err(KryptonError::NotWriteable.into());
-                }
-
-                // ensure profile_info PDA corresponds to authority_info
-                let (profile_pda, bump_seed) = get_profile_pda(authority_info.key, program_id);
-                if profile_pda != *profile_info.key {
-                    return Err(ProgramError::InvalidSeeds);
-                }
-
-                msg!("account checks complete");
-
-                // unpack token account to get amount
-                let token_account = TokenAccount::unpack(&token_account_info.try_borrow_data()?)?;
-                msg!("amount: {}, total: {}", args.amount, token_account.amount);
-
-                // ensure ATA has enough tokens to transfer
-                if token_account.amount < args.amount {
-                    return Err(ProgramError::InsufficientFunds.into());
-                }
-
-                msg!("transfering mint...");
-                let transfer_ix = transfer(
-                    token_program.key,
-                    token_account_info.key,
-                    dest_token_account_info.key,
-                    &profile_pda,
-                    &[&profile_pda],
-                    args.amount,
-                )?;
-                invoke_signed(
-                    &transfer_ix,
-                    &[
-                        token_program.clone(),
-                        token_account_info.clone(),
-                        dest_token_account_info.clone(),
-                        authority_info.clone(),
-                        profile_info.clone(),
-                    ],
-                    &[&[PDA_SEED, authority_info.key.as_ref(), &[bump_seed]]],
-                )?;
-                msg!("finished transfer of mint");
-
-                Ok(())
+                transfer_token::process_transfer_token(program_id, accounts, args)
             }
             KryptonInstruction::TransferNativeSOL(args) => {
                 msg!("Instruction: TransferNativeSOL");
